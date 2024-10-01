14b4053aacdb808def16a0b939198be45f03ea36
speranos <aoueldma@student.1337.ma>
2024-10-02 00:28:03 +0100

    env

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config({ path: process.cwd() + '/.env' }); 
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log("PORT ===== ",process.env.SERVER_PORT)
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
