12d0e50fb4615af228b8261de3b34d60f5caa5c2
speranos <aoueldma@student.1337.ma>
2024-10-04 07:30:50 +0100

    GG

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -7,7 +7,7 @@ import { CourseModule } from './course/course.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/MyMongo'),
+  imports: [MongooseModule.forRoot('mongodb://database:27017/MyMongo'),
             UserModule,
             CourseModule,
           ],
